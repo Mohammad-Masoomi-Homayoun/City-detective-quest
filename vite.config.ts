71297591798e566758deabd81ed885a536bd6472
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2026-05-13 20:54:33 +0200

    Added cert

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     port: 3000,
     host: true,
+    https: {
+      key: fs.readFileSync('.cert/key.pem'),
+      cert: fs.readFileSync('.cert/cert.pem'),
+    },
   },
   resolve: {
     alias: {
