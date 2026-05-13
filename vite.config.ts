8bc1134147da69a8f62456301b5b7781c5f10f98
Homa <mohammad.masoomihomayoun@theapsgroup.com>
2026-05-13 21:15:18 +0200

    fixed vercel cert skipped

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
+const httpsConfig = fs.existsSync('.cert/key.pem')
+  ? {
+      key: fs.readFileSync('.cert/key.pem'),
+      cert: fs.readFileSync('.cert/cert.pem'),
+    }
+  : undefined
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     host: true,
-    https: {
-      key: fs.readFileSync('.cert/key.pem'),
-      cert: fs.readFileSync('.cert/cert.pem'),
-    },
+    https: httpsConfig,
   },
   resolve: {
     alias: {
